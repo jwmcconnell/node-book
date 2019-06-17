6271e6fe78c08d7d3aa47f5446cbfb80d096f2b8
Jackmc <jackmc1122@gmail.com>
2019-06-17 16:40:53 -0700

    Call correct handler function from router.

--- router.js
+++ router.js
@@ -1,5 +1,8 @@
-function route(pathname) {
+function route(handle, pathname) {
   console.log(`About to route a request for ${pathname}`);
+  typeof handle[pathname] === 'function'
+    ? handle[pathname]()
+    : console.log(`No request handler found for ${pathname}`)
 }
 
 exports.route = route;
