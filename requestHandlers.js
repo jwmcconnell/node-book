b043b7720685200bfd4aac3727eba4e8182fa84c
Jackmc <jackmc1122@gmail.com>
2019-06-17 18:02:33 -0700

    upload, save and display image using formidable.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,8 @@
 let querystring = require('querystring');
+let fs = require('fs');
+let formidable = require('formidable');
 
-function start(res, postData) {
+function start(res, req) {
   console.log("Request handler 'start' was called");
 
   let body = '<html>' +
@@ -9,9 +11,9 @@ function start(res, postData) {
     'charset=UTF-8" />' +
     '</head>' +
     '<body>' +
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows="20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text" />' +
+    '<form action="/upload" enctype="multipart/form-data" method="post">' +
+    '<input type="file" name="upload" />' +
+    '<input type="submit" value="Upload file" />' +
     '</form>' +
     '</body>' +
     '</html>';
@@ -21,12 +23,34 @@ function start(res, postData) {
   res.end();
 }
 
-function upload(res, postData) {
-  console.log("Request handler 'upload' was called");
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write(`You've sent: ${querystring.parse(postData).text}`);
-  res.end();
+function upload(res, req) {
+  console.log("Request handler 'upload' was called.");
+
+  let form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(req, (error, fields, files) => {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, '/tmp/test.png', (error) => {
+      if (error) {
+        console.log('error')
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(`recieved image:<br/>`);
+    res.write(`<img src='/show' />`)
+    res.end();
+  });
+}
+
+function show(res) {
+  console.log("Request handler 'show' was called");
+  res.writeHead(200, { 'Content-Type': 'image/png' });
+  fs.createReadStream('/tmp/test.png').pipe(res);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
